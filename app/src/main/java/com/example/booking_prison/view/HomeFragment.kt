7358ddb7a79333a174702cba8b3c269bfa440cc2
c483481 <charleskojansow@gmail.com>
2023-11-26 02:15:09 +0800

    add data binding on home fragment

--- app/src/main/java/com/example/booking_prison/view/HomeFragment.kt
+++ app/src/main/java/com/example/booking_prison/view/HomeFragment.kt
@@ -6,13 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.booking_prison.R
+import com.example.booking_prison.databinding.FragmentHomeBinding
+import com.example.booking_prison.listener.HomeListener
 
 class HomeFragment : Fragment() {
+    lateinit var binding: FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater)
+
+        return binding.root
     }
 }
