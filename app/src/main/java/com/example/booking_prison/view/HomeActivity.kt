e7489498520bd19d59566068ca490d3082258745
c483481 <charleskojansow@gmail.com>
2023-11-25 23:11:24 +0800

    add bottom navigation on home activity

--- app/src/main/java/com/example/booking_prison/view/HomeActivity.kt
+++ app/src/main/java/com/example/booking_prison/view/HomeActivity.kt
@@ -2,11 +2,19 @@ package com.example.booking_prison.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.example.booking_prison.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = Navigation.findNavController(this, R.id.host_fragment)
+
+        NavigationUI.setupWithNavController(bottomNavigation, navController)
     }
 }
