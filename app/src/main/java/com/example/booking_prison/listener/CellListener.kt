2fc0f4d17e66ce13b117c2e69c687039bc2f47fe
c483481 <charleskojansow@gmail.com>
2023-11-27 02:41:50 +0800

    add call get list cell on view model

--- app/src/main/java/com/example/booking_prison/listener/CellListener.kt
+++ app/src/main/java/com/example/booking_prison/listener/CellListener.kt
@@ -1,8 +1,12 @@
 package com.example.booking_prison.listener
 
+import com.example.asisten_damkar.response.ResponseList
+import com.example.booking_prison.response.CellResponse
+
 interface CellListener {
     fun onClickBack()
     fun onClickAdd()
     fun onFetch()
     fun onFailed()
+    fun onSuccessFetch(data: ResponseList<CellResponse>)
 }
--- app/src/main/java/com/example/booking_prison/view_model/CellViewModel.kt
+++ app/src/main/java/com/example/booking_prison/view_model/CellViewModel.kt
@@ -3,11 +3,13 @@ package com.example.booking_prison.view_model
 import android.view.View
 import androidx.lifecycle.ViewModel
 import com.example.booking_prison.listener.CellListener
+import com.example.booking_prison.repository.CellRepository
 import com.example.booking_prison.utils.LoginUtils
 
 class CellViewModel: ViewModel() {
     lateinit var loginUtils: LoginUtils
     lateinit var cellListener: CellListener
+    private val cellRepository = CellRepository()
 
     fun onClickBackButton(view: View) {
         cellListener.onClickBack()
@@ -18,6 +20,16 @@ class CellViewModel: ViewModel() {
     }
 
     fun fetchCell() {
+        cellListener.onFetch()
 
+        val data = cellRepository.getListCell(loginUtils.getAccessToken())
+
+        data.observeForever {
+            if(it == null) {
+                cellListener.onFailed()
+                return@observeForever
+            }
+            cellListener.onSuccessFetch(it)
+        }
     }
 }
