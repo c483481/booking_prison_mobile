25c7f714ddfd0c87d82453f7f6f17bcdb9ecba4b
c483481 <charleskojansow@gmail.com>
2023-11-26 19:58:54 +0800

    add on check click

--- app/src/main/java/com/example/booking_prison/listener/HomeListener.kt
+++ app/src/main/java/com/example/booking_prison/listener/HomeListener.kt
@@ -1,5 +1,12 @@
 package com.example.booking_prison.listener
 
+import com.example.asisten_damkar.response.ResponseList
+import com.example.booking_prison.response.BookingResponse
+
 interface HomeListener {
     fun onClickBooking()
+
+    fun onFetchBooking()
+
+    fun onGetResult(data: ResponseList<BookingResponse>?)
 }
--- app/src/main/java/com/example/booking_prison/view_model/HomeViewModel.kt
+++ app/src/main/java/com/example/booking_prison/view_model/HomeViewModel.kt
@@ -3,11 +3,26 @@ package com.example.booking_prison.view_model
 import android.view.View
 import androidx.lifecycle.ViewModel
 import com.example.booking_prison.listener.HomeListener
+import com.example.booking_prison.repository.BookingRepository
+import com.example.booking_prison.utils.LoginUtils
 
 class HomeViewModel: ViewModel() {
     lateinit var homeListener: HomeListener
+    lateinit var loginUtils: LoginUtils
+    private val bookingRepository: BookingRepository = BookingRepository()
+
 
     fun onClickBookingButton(view: View) {
         homeListener.onClickBooking()
+    }
+
+    fun onClickCheckBooking(view: View) {
+        homeListener.onFetchBooking()
+
+        val result = bookingRepository.getListBooking(loginUtils.getAccessToken(), isUsers = true)
+
+        result.observeForever {
+            homeListener.onGetResult(it)
+        }
     }
 }
