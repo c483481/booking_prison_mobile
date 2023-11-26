7e9e32a4b22836f86867c2db02cec707846b9ba8
c483481 <charleskojansow@gmail.com>
2023-11-26 20:36:17 +0800

    add penjaga navigation

---
+++ app/src/main/java/com/example/booking_prison/view/HomePenjagaActivity.kt
@@ -0,0 +1,12 @@
+package com.example.booking_prison.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.booking_prison.R
+
+class HomePenjagaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_penjaga)
+    }
+}
