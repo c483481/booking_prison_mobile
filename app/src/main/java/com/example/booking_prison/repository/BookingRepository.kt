1c7453ca697ba2d894701e7cbd85e0640a0f349d
c483481 <charleskojansow@gmail.com>
2023-11-26 19:35:26 +0800

    add get list booking repository

--- app/src/main/java/com/example/booking_prison/repository/BookingRepository.kt
+++ app/src/main/java/com/example/booking_prison/repository/BookingRepository.kt
@@ -3,8 +3,10 @@ package com.example.booking_prison.repository
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.asisten_damkar.response.Response
+import com.example.asisten_damkar.response.ResponseList
 import com.example.booking_prison.network.BodyBooking
 import com.example.booking_prison.network.BookingNetwork
+import com.example.booking_prison.response.BookingResponse
 import retrofit2.Call
 import retrofit2.Callback
 
@@ -26,6 +28,36 @@ class BookingRepository {
                 data.value = false
             }
         })
+
+        return data
+    }
+
+    fun getListBooking(token: String, isUsers: Boolean? = null, limit: Int = 10, showAll: Boolean = false, notClear: Boolean? = null): LiveData<ResponseList<BookingResponse>?> {
+        val data = MutableLiveData<ResponseList<BookingResponse>?>()
+
+        bookingNetwork
+            .getListBooking(token, isUsers = isUsers, limit = limit, showAll = showAll, noClear = notClear)
+            .enqueue(object : Callback<Response<ResponseList<BookingResponse>>> {
+                override fun onResponse(
+                    call: Call<Response<ResponseList<BookingResponse>>>,
+                    response: retrofit2.Response<Response<ResponseList<BookingResponse>>>
+                ) {
+                    if(response.isSuccessful) {
+                        data.value = response.body()!!.data
+                        return
+                    }
+
+                    data.value = null
+                }
+
+                override fun onFailure(
+                    call: Call<Response<ResponseList<BookingResponse>>>,
+                    t: Throwable
+                ) {
+                    data.value = null
+                }
+
+            })
 
         return data
     }
