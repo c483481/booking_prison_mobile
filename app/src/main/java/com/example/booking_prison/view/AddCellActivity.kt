5de0c9f2e01153d076329e1c4051478411bb21d0
c483481 <charleskojansow@gmail.com>
2023-11-27 02:58:02 +0800

    add page for add cell

---
+++ app/src/main/java/com/example/booking_prison/view/AddCellActivity.kt
@@ -0,0 +1,12 @@
+package com.example.booking_prison.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.booking_prison.R
+
+class AddCellActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_cell)
+    }
+}
