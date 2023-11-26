7a5956035f7fcd7483f8d8febf45b0b775d98b11
c483481 <charleskojansow@gmail.com>
2023-11-27 04:29:06 +0800

    add default napi theme

---
+++ app/src/main/java/com/example/booking_prison/view/NapiActivity.kt
@@ -0,0 +1,12 @@
+package com.example.booking_prison.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.booking_prison.R
+
+class NapiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_napi)
+    }
+}
