2bbbeaea4bef52cf7fd7ccd742f8ce13775cdc0e
c483481 <charleskojansow@gmail.com>
2023-11-27 04:37:33 +0800

    fix napi callback

--- app/src/main/java/com/example/booking_prison/network/NapiNetwork.kt
+++ app/src/main/java/com/example/booking_prison/network/NapiNetwork.kt
@@ -3,6 +3,7 @@ package com.example.booking_prison.network
 import com.example.asisten_damkar.response.Response
 import com.example.asisten_damkar.response.ResponseList
 import com.example.booking_prison.response.BookingResponse
+import com.example.booking_prison.response.NapiResponse
 import com.example.booking_prison.utils.getBaseUrl
 import retrofit2.Call
 import retrofit2.Retrofit
@@ -18,7 +19,7 @@ interface NapiNetwork {
         @Query("filters[cellName]") cellName: String? = null,
         @Query("limit") limit: Int = 10,
         @Query("showAll") showAll: Boolean = false,
-    ): Call<Response<ResponseList<BookingResponse>>>
+    ): Call<Response<ResponseList<NapiResponse>>>
 
     companion object {
         operator fun invoke(): NapiNetwork {
