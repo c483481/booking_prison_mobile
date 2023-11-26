3f574495dee843a923ed5aad941518be9deacdbe
c483481 <charleskojansow@gmail.com>
2023-11-27 04:41:41 +0800

    add get list napi on repository

---
+++ app/src/main/java/com/example/booking_prison/repository/NapiRepository.kt
@@ -0,0 +1,38 @@
+package com.example.booking_prison.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.asisten_damkar.response.Response
+import com.example.asisten_damkar.response.ResponseList
+import com.example.booking_prison.network.NapiNetwork
+import com.example.booking_prison.response.NapiResponse
+import retrofit2.Call
+import retrofit2.Callback
+
+class NapiRepository {
+    private val napiNetwork = NapiNetwork()
+
+    fun getListNapi(token: String, showAll: Boolean= false, limit: Int = 10, cellName: String? = null): LiveData<ResponseList<NapiResponse>?> {
+        val data = MutableLiveData<ResponseList<NapiResponse>?>()
+
+        napiNetwork.getListNapi(token, showAll = showAll, limit = limit, cellName = cellName).enqueue(object : Callback<Response<ResponseList<NapiResponse>>> {
+            override fun onResponse(
+                call: Call<Response<ResponseList<NapiResponse>>>,
+                response: retrofit2.Response<Response<ResponseList<NapiResponse>>>
+            ) {
+                if(response.isSuccessful) {
+                    data.value = response.body()!!.data
+                    return
+                }
+                data.value = null
+            }
+
+            override fun onFailure(call: Call<Response<ResponseList<NapiResponse>>>, t: Throwable) {
+                data.value = null
+            }
+        })
+
+
+        return data
+    }
+}
