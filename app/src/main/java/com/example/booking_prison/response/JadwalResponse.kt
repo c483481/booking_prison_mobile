9001fba40a83ca40256a9cce603800c521064ebc
c483481 <charleskojansow@gmail.com>
2023-11-27 20:01:12 +0800

    add jadwal response

---
+++ app/src/main/java/com/example/booking_prison/response/JadwalResponse.kt
@@ -0,0 +1,11 @@
+package com.example.booking_prison.response
+
+data class JadwalResponse(
+    val jumat: String,
+    val kamis: String,
+    val minggu: String,
+    val rabu: String,
+    val sabtu: String,
+    val selasa: String,
+    val senin: String
+)
